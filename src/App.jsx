5178bf062e9409a7aa32f6854d851142d6f87c82
enano17711 <enano17711@gmail.com>
2023-02-16 12:15:24 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header.jsx";
+
+function App() {
+
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NuevoPresupuesto = () => {
+    return (
+        <div>
+            <h2>Nuevo Presupuesto</h2>
+        </div>
+    );
+};
+
+export default NuevoPresupuesto;
